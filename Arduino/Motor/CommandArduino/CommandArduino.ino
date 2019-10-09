fc200ef907a7c9cce08762f26232729d697af3e7
Unknown <stephenthomasfelix@yahoo.com>
2019-10-09 14:40:42 +0800

    FileUpload

---
+++ Arduino/Motor/CommandArduino/CommandArduino.ino
@@ -0,0 +1,437 @@
+#include "DualVNH5019MotorShield.h"
+#include "Sensors.h"
+#include "math.h"
+
+DualVNH5019MotorShield md;
+
+const long Pi = 3.1328;
+long E2A = 3;
+long E2B = 5;
+long E1A = 11;
+long E1B = 13;
+float M1k = 0.386704;
+double M1ts = 0.00375;//78,0.00565
+double M1td = 0.048;//0.07625;
+float M1k1, M1k2, M1k3;
+float M2k = 0.381448;
+float M2ts = 0.0045;//77;
+float M2td = 0.054;
+float M2k1, M2k2, M2k3;
+bool update1, update2;
+String sensorData;
+float errorCtrl = 2;
+float error1[3] = {0, 0, 0};
+float error2[3] = {0, 0, 0};
+float setSpeed1, setSpeed2, angle = 90;
+float rpm1 = 100, rpm2 = 100;
+long startTime, stopTime;
+float calibrationOffset = -30;
+float turnOffset = 25;
+#define ledPin 13
+int timer1_counter;
+float dist[3] = {30, 30, 30};
+
+
+void setup() {
+  Serial.begin(115200);
+  //  Serial.println("Dual VNH5019 Motor Shield");
+  pinMode(E1A, INPUT);
+  pinMode(E1B, INPUT);
+  pinMode(E2A, INPUT);
+  pinMode(E2B, INPUT);
+  PIDGearController1();
+  PIDGearController2();
+
+  //  Serial.print("Speed set : ");
+  //  Serial.println(rpm1);
+
+  setSpeed1 = 0;
+  setSpeed2 = 0;
+
+  md.init();           // enable all interrupts
+}
+
+void loop() {
+  moveForward(10);
+  delay(4000);
+//  turnRight(180);
+//  delay(4000);
+//  moveForward(120);
+//  delay(4000);
+//  turnLeft(180);
+//  delay(4000);
+}
+
+
+//void loop() {
+//
+//  while (Serial.available() > 0) //RPi to Arduino
+//  {
+//    String input = Serial.readStringUntil('~'); //use this if char reading fails
+//    String firstVal = input;
+//    int secondVal = 10;
+//
+//    for (int i = 0; i < input.length(); i++) {
+//      if (input.substring(i, i + 1) == ":") {
+//        firstVal = input.substring(0, i);
+//        secondVal = input.substring(i + 1).toInt();
+//        break;
+//      }
+//    }
+//
+//    char instructions = firstVal[0];
+//    switch (instructions) {
+//      //botstart88
+//      case 'S':
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //moveforward
+//      case '0':
+//        //Serial.println("X Bot forward");
+//        moveForward(secondVal);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //turn 90 deg left
+//      case '1':
+//        //Serial.println("X Bot turn left");
+//        turnRight(90);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //turn 90 deg right
+//      case '2':
+//        //Serial.println("X Bot turn right");
+//        turnLeft(90);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //bot backward
+//      case '3':
+//        //Serial.println("X Bot stopped");
+//        moveBackward(secondVal);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //turn 180 deg leftt
+//      case '4':
+//        Serial.println("X Bot turn 180 deg left");
+//        turnRight(180);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //turn 180 deg right
+//      case '5':
+//        //Serial.println("X Bot turn 180 deg right");
+//        turnLeft(180);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //botstop
+//      case 'E':
+//        //Serial.println("X Bot stopped");
+//        md.setM1Speed(0);
+//        md.setM2Speed(0);
+//        sensorData = returnSensorData();
+//        Serial.println("X_SENDATA"+sensorData);
+//        break;
+//      //calibration
+//      case 'C':
+//        Serial.println("X Bot calibrating");
+//        //code for calibrate
+//        break;
+//    }
+//  }
+//}
+void avoidObstacle(float distance) {
+  long runningTime = round(runTime(rpm1, distance));
+  Serial.print("Run Time : ");
+  Serial.println(runningTime);
+  startTime = millis();
+  stopTime = millis();
+  int count = 0;
+  while (stopTime - startTime < runningTime) {
+    if (count % 15 == 0) {
+      //      dist[2] = dist[1];
+      //      dist[1] = dist[0];
+      dist[0] = SR1.getDistance(false) + 2;
+      Serial.println(dist[0]);
+      //      float avgDist = (dist[0] + dist[1] + dist[2]) / 3.0;
+      //      Serial.print("Average Distance : ");
+      //      Serial.println(avgDist);
+      if ( dist[0] <= 20) {
+        Serial.println(dist[0]);
+        stopMovement();
+        float collisionDistance = returnSrDist (20 , SR1);
+        float travelDistance = sqrt(pow(collisionDistance, 2) + 256) + 30;
+        Serial.println(travelDistance);
+        delay(500);
+        turnRight(45);
+        delay(500);
+        moveForward(travelDistance );
+        delay(500);
+        turnLeft(90);
+        delay(500);
+        moveForward(travelDistance + 7.5);
+        delay(500);
+        turnRight(45);
+        delay(500);
+      }
+    } count++;
+    setSpeed1 = setWheelSpeed1(rpm1, true);
+    setSpeed2 = setWheelSpeed2(rpm2, true);
+    if (update1)
+      md.setM1Speed(setSpeed1);
+    if (update2)
+      md.setM2Speed(setSpeed2);
+    stopTime = millis();
+  }
+  md.setM1Speed(0);
+  md.setM2Speed(0);
+}
+
+float caluclateExtraSpeed(long collisionTime, float collisionDistance) {
+  float botRadius = 9;
+  float angle = atan(15 / collisionDistance);
+  float turnDistance = botRadius * angle;
+  return turnDistance / collisionTime;
+
+}
+
+void moveForward(float distance) {
+  long runningTime = round(runTime(rpm1, distance));
+  Serial.print("Run Time : ");
+  Serial.println(runningTime);
+  startTime = millis();
+  stopTime = millis();
+  while (stopTime - startTime < runningTime) {
+    Serial.println(stopTime - startTime);
+    setSpeed1 = setWheelSpeed1(rpm1, true);
+    setSpeed2 = setWheelSpeed2(rpm2, true);
+    if (update1)
+      md.setM1Speed(setSpeed1);
+    if (update2)
+      md.setM2Speed(setSpeed2);
+    stopTime = millis();
+  }
+  md.setM1Brake(400);
+  md.setM2Brake(400);
+}
+
+void moveBackward(float distance) {
+  long runningTime = round(runTime(rpm1, distance));
+  //  Serial.print("Run Time : ");
+  //  Serial.println(runningTime);
+  startTime = millis();
+  stopTime = millis();
+  while (stopTime - startTime < runningTime) {
+    setSpeed1 = setWheelSpeed1(rpm1, false);
+    setSpeed2 = setWheelSpeed2(rpm2, false);
+    if (update1)
+      md.setM1Speed(setSpeed1);
+    if (update2)
+      md.setM2Speed(setSpeed2);
+    stopTime = millis();
+  }
+  md.setM1Brake(400);
+  md.setM2Brake(400);
+}
+
+void stopMovement() {
+  md.setM1Speed(0);
+  md.setM2Speed(0);
+}
+
+void turnLeft(float angle) {
+  long rotateTime = round(rotateArduino(rpm1, angle));
+  //  Serial.print("Rotation Time : ");
+  //  Serial.println(rotateTime);
+  startTime = millis();
+  stopTime = millis();
+  while (stopTime - startTime < rotateTime) {
+    setSpeed1 = setWheelSpeed1(rpm1, true);
+    setSpeed2 = setWheelSpeed2(rpm2, false);
+    if (update1)
+      md.setM1Speed(setSpeed1);
+    if (update2)
+      md.setM2Speed(setSpeed2);
+    stopTime = millis();
+  }
+  md.setM1Brake(400);
+  md.setM2Brake(400);
+}
+
+void turnRight(float angle) {
+  long rotateTime = round(rotateArduino(rpm1, angle));
+  //  Serial.print("Rotation Time : ");
+  //  Serial.println(rotateTime);
+  startTime = millis();
+  stopTime = millis();
+  while (stopTime - startTime <  rotateTime) {
+    setSpeed1 = setWheelSpeed1(rpm1, false);
+    setSpeed2 = setWheelSpeed2(rpm2, true);
+    if (update1)
+      md.setM1Speed(setSpeed1);
+    if (update2)
+      md.setM2Speed(setSpeed2);
+    stopTime = millis();
+  }
+  md.setM1Brake(400);
+  md.setM2Brake(400);
+}
+
+float runTime(float rpm, float distance) {
+  float wheelRadius = 3;
+  float Speed = (rpm * 2 * Pi * wheelRadius) / 60000.0; // Speed in cm/millisecond
+  float runTime = distance / Speed;
+  return runTime + 1465;//+ 1428 ; //705.375 ;
+}
+
+
+float rotateArduino(float rpm, float angle) {
+  float radius = 9;
+  float wheelRadius = 3;
+  float distance = (2 * Pi * radius * angle) / 360.0;
+  float Speed = (rpm * 2 * Pi * wheelRadius) / 60000.0; // Speed in cm/millisecond
+  float rotateTime = distance / Speed;
+  return rotateTime + 1428 ;//1700; //
+}
+
+float setWheelSpeed1(float rpm, bool forward) {
+
+  float motorSpeed1 = getSpeed (pulseIn(E1A, HIGH));
+
+  //  Serial.print(motorSpeed1);
+  //  Serial.print(" | ");
+  
+  if (motorSpeed1 > 150)
+    motorSpeed1 = 0;
+
+  errorUpdate(error1);
+  error1[0] = rpm - motorSpeed1 ;
+
+  if (fabs(error1[0]) > errorCtrl ) {
+    update1 = true;
+    if (motorSpeed1 < 150)
+      setSpeed1 = correctedSpeed(fabs(setSpeed1), error1[0], error1[1], error1[2], 1);
+    else
+      error1[0] = 0;
+    return forward ? setSpeed1 : -setSpeed1;
+  }
+  
+  update1 = false;
+}
+
+float setWheelSpeed2(float rpm, bool forward) {
+
+  float motorSpeed2 = getSpeed (pulseIn(E2B, HIGH));
+  //  Serial.println(motorSpeed2);
+  if (motorSpeed2 > 150)
+    motorSpeed2 = 0;
+
+  errorUpdate(error2);
+  error2[0] = rpm - motorSpeed2 ;
+
+
+  if (fabs(error2[0]) > errorCtrl ) {
+    update2 = true;
+    if (motorSpeed2 < 150)
+      setSpeed2 = correctedSpeed(fabs(setSpeed2), error2[0], error2[1], error2[2], 2);
+    else
+      error2[0] = 0;
+
+    return forward ? setSpeed2 : -setSpeed2;
+  }
+  update2 = false;
+}
+
+float getSpeed (long pulseInTime) {
+  long timePeriod = pulseInTime * 2;
+  float rps = 1000000 / (timePeriod * 562.25);
+  float rpm = rps * 60;
+  return rpm;
+}
+
+void errorUpdate(float error[]) {
+  error[2] = error[1];
+  error[1] = error[0];
+}
+
+void PIDGearController1() {
+
+  float Kc =  (1.2 * M1ts) / (M1k * M1td);
+  float Ti =  2 * M1td;
+  float Td = 0.5 * M1td;
+  //  float Td = 0;
+
+  //  Serial.print("Kc : ");
+  //  Serial.print(Kc);
+  //  Serial.print(" Ti : ");
+  //  Serial.print(Ti);
+  //  Serial.print(" Td : ");
+  //  Serial.println(Td);
+
+  float Kp =  Kc;
+  float Ki = Kc / Ti;
+  //  float Ki = 0;
+  //  float Kd = 0;
+  float Kd =  Kc * Td;
+
+  M1k1 = Kp + Ki + Kd;
+  M1k2 = -Kp - 2 * Kd;
+  //  M1k2 = 0;
+  M1k3 = Kd;
+  //
+  //  Serial.print("K1 : ");
+  //  Serial.print(M1k1);
+  //  Serial.print(" K2 : ");
+  //  Serial.print(M1k2);
+  //  Serial.print(" K3 : ");
+  //  Serial.println(M1k3);
+}
+
+void PIDGearController2() {
+  float Kc =  (1.2 * M2ts) / (M2k * M2td);
+  float Ti = 2 * M2td;
+  float Td = 0.5 * M2td;
+
+  float Kp =  Kc;
+  float Ki = Kc / Ti;
+  //  float Ki = 0;
+  //  float Kd = 0;
+  float Kd =  Kc * Td;
+
+  M2k1 = Kp + Ki + Kd;
+  M2k2 = -Kp - 2 * Kd;
+  M2k3 = Kd;
+
+  //  Serial.print("K1 : ");
+  //  Serial.print(M2k1);
+  //  Serial.print(" K2 : ");
+  //  Serial.print(M2k2);
+  //  Serial.print(" K3 : ");
+  //  Serial.println(M2k3);
+}
+
+float correctedSpeed(float originalSpeed, float error1, float error2, float error3, int motor) {
+
+  if (motor == 1) {
+    //    Serial.print(" Additional Speed : ");
+    //    Serial.println(error1 * M1k1 + error2 * M1k2 + error3 * M1k3);
+    float newSpeed = originalSpeed + error1 * M1k1 + error2 * M1k2 + error3 * M1k3;
+    if (newSpeed > 350)
+      newSpeed = 350;
+
+    return newSpeed;
+  }
+  else {
+    //    Serial.print(" Additional Speed : ");
+    //    Serial.print(error1 * M2k1);
+    float newSpeed = originalSpeed + error1 * M2k1 + error2 * M2k2 + error3 * M2k3;
+    if (newSpeed > 400)
+      newSpeed = 350;
+
+    return newSpeed;
+  }
+}
---
+++ Arduino/Motor/Motor.ino
@@ -0,0 +1,395 @@
+
+#include "DualVNH5019MotorShield.h"
+#include "TimerOne.h"
+
+DualVNH5019MotorShield md;
+long E2A = 3;
+long E2B = 5;
+long E1A = 11;
+long E1B = 13;
+float avgSpeed1 = 0;
+float avgSpeed2 = 0;
+float avgSpeeds1[25];
+float avgSpeeds2[25];
+int count1 = 0;
+int count2 = 0;
+int timer1_counter;
+float M1k = 0.386704;
+double M1ts = 0.00375;//78,0.00565
+double M1td = 0.047;//0.07625;
+float M1k1, M1k2, M1k3;
+float M2k = 0.381448;
+float M2ts = 0.0043;//77;
+float M2td = 0.049;
+float M2k1, M2k2, M2k3;
+float errorCtrl = 2;
+float error1[3] = {0, 0, 0};
+float error2[3] = {0, 0, 0};
+float setSpeed1, setSpeed2, rpm = 100, rpm1, rpm2;
+#define ledPin 13
+
+void stopIfFault()
+{
+  if (md.getM1Fault())
+  {
+    Serial.println("M1 fault");
+    while (1);
+  }
+  if (md.getM2Fault())
+  {
+    Serial.println("M2 fault");
+    while (1);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Dual VNH5019 Motor Shield");
+  pinMode(E1A, INPUT);
+  pinMode(E1B, INPUT);
+  pinMode(E2A, INPUT);
+  pinMode(E2B, INPUT);
+  PIDGearController1();
+  PIDGearController2();
+
+  Serial.print("Speed set : ");
+  Serial.println(rpm);
+
+  setSpeed1 = 0;
+  setSpeed2 = 0;
+
+  md.init();
+
+  md.setM1Speed(setSpeed1);
+  md.setM2Speed(setSpeed2);
+}
+
+void findSpeed(void)
+{
+  float speed_E1A, speed_E2A, speed_E1B, speed_E2B;
+  speed_E1A = getSpeed(pulseIn(E1A, HIGH));
+  speed_E1B = getSpeed(pulseIn(E1B, HIGH));
+  speed_E2A = getSpeed(pulseIn(E2A, HIGH));
+  speed_E2B = getSpeed(pulseIn(E2B, HIGH));
+
+  Serial.print(millis());
+  Serial.print(", ");
+  //  Serial.print("M1 : ");
+  Serial.print((speed_E1B + speed_E1A) / 2);
+  Serial.print(", ");
+  //  Serial.print("M2 : ");
+  Serial.println((speed_E2B + speed_E2A) / 2);
+}
+
+
+void loop() {
+
+  //  Serial.print("Set Speed 1 : ");
+  //  Serial.print(setSpeed1);
+  //  Serial.print("Set Speed 2 : ");
+  //  Serial.println(setSpeed2);
+
+
+  delay(50);
+
+  float motorSpeed1 = getSpeed (pulseIn(E1B, HIGH));
+  float motorSpeed2 = getSpeed (pulseIn(E2A, HIGH));
+//    Serial.print(", Motor Speed 1 : ");
+  Serial.print(motorSpeed1);
+  Serial.print(" | ");
+////    Serial.print(", Motor Speed 2 : ");
+  Serial.print(motorSpeed2);
+  Serial.print(" | ");
+//  Serial.print(motorSpeed2 - motorSpeed1);
+
+  if(fabs(rpm - motorSpeed2)>errorCtrl)
+  Serial.println(" ~~~");
+  else
+  Serial.println();
+
+  if (motorSpeed1 > 150)
+    motorSpeed1 = 0;
+
+  if (motorSpeed2 > 150)
+    motorSpeed2 = 0;
+
+  errorUpdate(error1);
+  error1[0] = rpm - motorSpeed1 ;
+  errorUpdate(error2);
+  error2[0] = rpm - motorSpeed2 ;
+
+  if (fabs(error1[0]) > errorCtrl ) {
+    //    Serial.print(millis());
+
+    if (motorSpeed1 < 150)
+      setSpeed1 = correctedSpeed(setSpeed1, error1[0], error1[1], error1[2], 1);
+    else
+      error1[0] = 0;
+
+    md.setM1Speed(setSpeed1);
+    //      Serial.print("New Set Speed 1 : ");
+    //      Serial.println(setSpeed1);
+    //      Serial.println();
+  }
+
+  if (fabs(error2[0]) > errorCtrl) {
+    //      Serial.print(millis());
+
+    if (motorSpeed2 < 150)
+      setSpeed2 = correctedSpeed(setSpeed2, error2[0], error2[1], error2[2], 2);
+    else
+      error2[0] = 10;
+
+    md.setM2Speed(setSpeed2);
+    //      Serial.print(", New Set Speed 2 : ");
+    //      Serial.println(setSpeed2);
+  }
+
+}
+
+//void loop() {
+//  Serial.println("Speed set : 250");
+//  md.setM1Speed(250);
+//  md.setM2Speed(250);
+//  for (int i = 0; i < 16000; i++)
+//    if (i % 100 == 0)
+//    { findSpeed();
+//      delay(10);
+//    }
+//  Serial.print(millis());
+//  Serial.print(", ");
+//  Serial.println("Speed set : 300");
+//  md.setM1Speed(300);
+//  md.setM2Speed(300);
+//  for (int i = 0; i < 16000; i++)
+//    if (i % 100 == 0)
+//    { findSpeed();
+//      delay(10);
+//    }
+//  Serial.println("Speed set : 0");
+//  md.setM1Speed(0);
+//  md.setM2Speed(0);
+//  delay(100000);
+//}
+//
+//void loop()
+//{ float speed_E1A, speed_E2A, speed_E1B, speed_E2B;
+//  for (int j = 1; j <= 8; j++) {
+//    resetAvgSpeed(1);
+//    //resetAvgSpeed(2);
+//    long speed = j * 50;
+//    for (int i = 0; i <= 2000; i++)
+//    {
+//      md.setM1Speed(speed);
+//      //md.setM2Speed(speed);
+//      stopIfFault();
+//      if (i % 200 == 100)
+//      {
+//        speed_E1A = getSpeed(pulseIn(E1A, HIGH));
+//        speed_E1B = getSpeed(pulseIn(E1B, HIGH));
+//        addAvgSpeed(1, speed_E1A);
+//        addAvgSpeed(1, speed_E1B);
+//
+////        speed_E2A = getSpeed(pulseIn(E2A, HIGH));
+////        speed_E2B = getSpeed(pulseIn(E2B, HIGH));
+////        addAvgSpeed(2, speed_E2A);
+////        addAvgSpeed(2, speed_E2B);
+//      }
+//      delay(2);
+//    }
+//    Serial.print("Speed : " );
+//    Serial.println(speed);
+//    Serial.print("M1 : ");
+//    Serial.print(getAvgSpeed(1));
+//    Serial.print(", ");
+//    Serial.println(sortAndAverage(avgSpeeds1, count1));
+////    Serial.print("M2 : ");
+////    Serial.print(getAvgSpeed(2));
+////    Serial.print(", ");
+////    Serial.println(sortAndAverage(avgSpeeds2, count2));
+//  }
+//
+//}
+
+void errorUpdate(float error[]) {
+  error[2] = error[1];
+  error[1] = error[0];
+}
+
+//float setSpeedMotor1 (float rpm) {
+//  return (rpm + 9.156) / 0.37082;
+//}
+//
+//
+//float setSpeedMotor2 (float rpm) {
+//  return (rpm + 12.133) / 0.36246;
+//}
+
+float getSpeed (long pulseInTime) {
+  long timePeriod = pulseInTime * 2;
+  float rps = 1000000 / (timePeriod * 562.25);
+  float rpm = rps * 60;
+  return rpm;
+}
+
+float addAvgSpeed (int motor, float rpm) {
+  if (motor == 1)
+  {
+    avgSpeed1 += rpm;
+    avgSpeeds1[count1] = rpm;
+    count1++;
+  }
+  else if (motor == 2)
+  {
+    avgSpeed2 += rpm;
+    avgSpeeds2[count2] = rpm;
+    count2++;
+  }
+  else
+    Serial.print("Invalid input of motor");
+}
+
+float getAvgSpeed (int motor) {
+  if (motor == 1)
+    return avgSpeed1 / count1;
+  else if (motor == 2)
+    return avgSpeed2 / count2;
+  else
+    Serial.print("Invalid input of motor");
+}
+
+float resetAvgSpeed (int motor) {
+  if (motor == 1)
+  {
+    avgSpeed1 = 0;
+    count1 = 0;
+  }
+  else if (motor == 2)
+  {
+    avgSpeed2 = 0;
+    count2 = 0;
+  }
+  else
+    Serial.print("Invalid input of motor");
+}
+
+float sortAndAverage(float values[], int count) {
+  sort(values, count);
+  int startCount = 2 * count / 5;
+  int stopCount = 4 * count / 5;
+  float sum = 0;
+  for (int i = startCount; i <= stopCount; i++)
+    sum += values[i];
+  return sum / (stopCount - startCount + 1);
+}
+
+void sort(float values[], int count) {
+  float temp;
+  for (int i = 0; i < count; i++)
+    for (int j = i + 1; j <= count - 1 - i; j++)
+      if (values[i] > values[j])
+      { temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+      }
+}
+
+void PIDGearController1() {
+
+  Serial.print("M1k : ");
+  Serial.print(M1k);
+  Serial.print(" M1ts : ");
+  Serial.print(M1ts);
+  Serial.print(" M1td : ");
+  Serial.println(M1td);
+
+  float Kc =  (1.2 * M1ts) / (M1k * M1td);
+  float Ti =  2 * M1td;
+  float Td = 0.5 * M1td;
+  //  float Td = 0;
+
+  Serial.print("Kc : ");
+  Serial.print(Kc);
+  Serial.print(" Ti : ");
+  Serial.print(Ti);
+  Serial.print(" Td : ");
+  Serial.println(Td);
+
+  float Kp =  Kc;
+  float Ki = Kc / Ti;
+  //  float Ki = 0;
+  //  float Kd = 0;
+  float Kd =  Kc * Td;
+
+  Serial.print("Kp : ");
+  Serial.print(Kp);
+  Serial.print(" Ki : ");
+  Serial.print(Ki);
+  Serial.print(" Kd : ");
+  Serial.println(Kd);
+
+  M1k1 = Kp + Ki + Kd;
+  M1k2 = -Kp - 2 * Kd;
+  //  M1k2 = 0;
+  M1k3 = Kd;
+
+  Serial.print("K1 : ");
+  Serial.print(M1k1);
+  Serial.print(" K2 : ");
+  Serial.print(M1k2);
+  Serial.print(" K3 : ");
+  Serial.println(M1k3);
+}
+
+void PIDGearController2() {
+  float Kc =  (1.2 * M2ts) / (M2k * M2td);
+  float Ti = 2 * M2td;
+  float Td = 0.5 * M2td;
+
+  float Kp =  Kc;
+  float Ki = Kc / Ti;
+  //  float Ki = 0;
+  //  float Kd = 0;
+  float Kd =  Kc * Td;
+
+  M2k1 = Kp + Ki + Kd;
+  M2k2 = -Kp - 2 * Kd;
+  M2k3 = Kd;
+
+  Serial.print("K1 : ");
+  Serial.print(M2k1);
+  Serial.print(" K2 : ");
+  Serial.print(M2k2);
+  Serial.print(" K3 : ");
+  Serial.println(M2k3);
+}
+
+float correctedSpeed(float originalSpeed, float error1, float error2, float error3, int motor) {
+  //  Serial.print(" original Speed : ");
+  //  Serial.print(originalSpeed);
+  //  Serial.print(" Error 1 : ");
+  //  Serial.print(error1);
+  //  Serial.print(" Error 2 : ");
+  //  Serial.print(error2);
+  //  Serial.print(" Error 3 : ");
+  //  Serial.print(error3);
+  if (motor == 1) {
+    //    Serial.print(" Additional Speed : ");
+    //    Serial.println(error1 * M1k1 + error2 * M1k2 + error3 * M1k3);
+    float newSpeed = originalSpeed + error1 * M1k1 + error2 * M1k2 + error3 * M1k3;
+    if (newSpeed > 350)
+      newSpeed = 350;
+
+    return newSpeed;
+  }
+  else {
+    //    Serial.print(" Additional Speed : ");
+    //    Serial.print(error1 * M2k1);
+    float newSpeed = originalSpeed + error1 * M2k1 + error2 * M2k2 + error3 * M2k3;
+    if (newSpeed > 400)
+      newSpeed = 350;
+
+    return newSpeed;
+  }
+}
